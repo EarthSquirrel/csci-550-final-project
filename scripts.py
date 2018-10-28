17e4a181a8e3b9b3b372250e8e1e837c83402cbf
Henry <hsoule427@gmail.com>
2018-10-27 19:32:54 -0600

    New monthly_avg function works

--- scripts.py
+++ scripts.py
@@ -59,18 +59,114 @@ def _get_monthly_avg(month, dataframe, city):
 #END FUNCTION
 
 def get_monthly_avg(month, dataframe, city):
+
 	vals = [] #Set of all monthy values of a specific weather attribute for one city
 	for i in humidity_df.index:
-		date = pd.to_datetime(humidity_df.loc[i,'datetime'])
-		if (date.month == month): #If we are in the right month
-			if (np.isnan(humidity_df.loc[i, city])): #If current value is empty, just add 0
+		date = pd.to_datetime(dataframe.loc[i,'datetime'])
+		if date.month == month: #If we are in the right month
+			if (np.isnan(dataframe.loc[i, city])): #If current value is empty, just add 0
 				vals.append(0)
 			else: #Otherwise add current value to vals
-				vals.append(humidity_df.loc[i, city])
+				vals.append(dataframe.loc[i, city])
 		else: #Once we've reached end of current month, jump ahead one year
 			i += jump_one_year(date)
 
 	return sum(vals) / len(vals)
+
+def get_monthly_avgs(dataframe, city):
+	jan_sum = 0
+	jan_count = 0
+	feb_sum = 0
+	feb_count = 0
+	mar_sum = 0
+	mar_count = 0
+	apr_sum = 0
+	apr_count = 0
+	may_sum = 0
+	may_count = 0
+	jun_sum = 0
+	jun_count = 0
+	jul_sum = 0
+	jul_count = 0
+	aug_sum = 0
+	aug_count = 0
+	sep_sum = 0
+	sep_count = 0
+	octo_sum = 0
+	octo_count = 0
+	nov_sum = 0
+	nov_count = 0
+	dec_sum = 0
+	dec_count = 0
+
+	for i in dataframe.index:
+		date = pd.to_datetime(dataframe.loc[i, 'datetime'])
+		if np.isnan(dataframe.loc[i, city]): continue
+		if date.month == 1:
+			jan_sum += dataframe.loc[i, city]
+			jan_count += 1
+		
+		elif date.month == 2:
+			feb_sum += dataframe.loc[i, city]
+			feb_count += 1
+		
+		elif date.month == 3:
+			mar_sum += dataframe.loc[i, city]
+			mar_count += 1
+		
+		elif date.month == 4:
+			apr_sum += dataframe.loc[i, city]
+			apr_count += 1
+		
+		elif date.month == 5:
+			may_sum += dataframe.loc[i, city]
+			may_count += 1
+		
+		elif date.month == 6:
+			jun_sum += dataframe.loc[i, city]
+			jun_count += 1
+		
+		elif date.month == 7:
+			jul_sum += dataframe.loc[i, city]
+			jul_count += 1
+		
+		elif date.month == 8:
+			aug_sum += dataframe.loc[i, city]
+			aug_count += 1
+		
+		elif date.month == 9:
+			sep_sum += dataframe.loc[i, city]
+			sep_count += 1
+		
+		elif date.month == 10:
+			octo_sum += dataframe.loc[i, city]
+			octo_count += 1
+		
+		elif date.month == 11:
+			nov_sum += dataframe.loc[i, city]
+			nov_count += 1
+		
+		elif date.month == 12:
+			dec_sum += dataframe.loc[i, city]
+			dec_count += 1
+	#END LOOP
+	
+	return [jan_sum / jan_count,
+			feb_sum / feb_count,
+			mar_sum / mar_count,
+			apr_sum / apr_count,
+			may_sum / mar_count,
+			jun_sum / jun_count,
+			jul_sum / jul_count,
+			aug_sum / aug_count,
+			sep_sum / sep_count,
+			octo_sum / octo_count,
+			nov_sum / nov_count,
+			dec_sum / dec_count]
+
+
+
+
 
 
 #Create a set of all monthly avgs of a specific attribute
@@ -123,20 +219,34 @@ def jump_one_year(date):
 # print(make_column(humidity_df))
 # print(datetime.now() - start_time)
 
-humidity_col = mp.Queue()
-lock = mp.Lock()
+# humidity_col = mp.Queue()
+# lock = mp.Lock()
 
-p1 = mp.Process(target=get_city_avgs, args=(lock, 'Vancouver', humidity_df, humidity_col))
-p2 = mp.Process(target=get_city_avgs, args=(lock, 'Portland', humidity_df, humidity_col))
-# t3 = mp.Process(target=get_city_avgs, args=['San Francisco', humidity_df, humidity_col])
+# p1 = mp.Process(target=get_city_avgs, args=(lock, 'Vancouver', humidity_df, humidity_col))
+# p2 = mp.Process(target=get_city_avgs, args=(lock, 'Portland', humidity_df, humidity_col))
+# # t3 = mp.Process(target=get_city_avgs, args=['San Francisco', humidity_df, humidity_col])
+# start_time = datetime.now()
+# p1.start()
+# p2.start()
+
+# p1.join()
+# p2.join()
+
+# print(humidity_col.get())
+# print(datetime.now() - start_time)
+humidity_col = []
+pressure_col = []
+temperature_col = []
+wind_direction_col = []
+wind_speed_col = []
 start_time = datetime.now()
-p1.start()
-p2.start()
-
-p1.join()
-p2.join()
-
-print(humidity_col.get())
+for city in columns[1:]:
+	print(city + ":")
+	humidity_col.append(get_monthly_avgs(humidity_df, city))
+	pressure_col.append(get_monthly_avgs(pressure_df, city))
+	temperature_col.append(get_monthly_avgs(temperature_df, city))
+	wind_direction_col.append(get_monthly_avgs(wind_direction_df, city))
+	wind_speed_col.append(get_monthly_avgs(wind_speed_df, city))
 print(datetime.now() - start_time)
 
 
