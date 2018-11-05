65fd5f9d345f62847e232da2793dbf52ae10cd9f
Henry <hsoule427@gmail.com>
2018-11-04 19:39:36 -0700

    Fixed a bug that took me way to long to figure out...

--- mean_shift.py
+++ mean_shift.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import numpy as np
 import math
+import matplotlib.pyplot as plt
+from datetime import datetime
+# from sklearn.neighbors import BallTree
 
 kernel_bandwidth = 4
 
@@ -9,50 +12,60 @@ def distance(x, xi):
 	return np.sqrt(np.sum((x-xi)**2))
 
 
-def neighborhood_pts(X, x_centroid, radius=5):
+def neighborhood_pts(X, x_centroid, columns, radius=6):
 	neighbors = []
 	
 	for i,row in X.iterrows():
 		#Skip don't add x_centroid to the neighborhood
 		if row.name == x_centroid.name: continue
-		if distance(row, x_centroid) <= radius:
+		# print(row.name,distance(row[columns], x_centroid[columns]))
+		if distance(row[columns], x_centroid[columns]) <= radius:
 			neighbors.append(row)
 		
-
 	return neighbors
 
 
 def gauss_kernel(bandwidth, distance):
-	return (1 / (bandwidth * np.sqrt(2 * math.pi))) * np.exp(-0.5 * ((distance / bandwidth)**2))
+	d = (bandwidth * np.sqrt(2 * math.pi) * np.exp(-0.5 * ((distance / bandwidth)**2)))
+	print(d)
+	return (1 / d)
 
 #TODO pass columns array as a parameter
-def mean_shift(data, column):
-	X = data.copy()
-	iterations = 5
+def mean_shift(data, columns):
+	X = data[columns].copy()
+	iterations = 1
 	past_X = []
 	break_loop = False
 	for it in range(iterations):
 		for i,x in X.iterrows():
 			#Find neighbors
-			neighbors = neighborhood_pts(X, x)
+			neighbors = neighborhood_pts(X, x, columns)
 
-			#Find m(x) for each x in neighbors
 			numerator = 0
 			denominator = 0
+			
 			for xi in neighbors:
-				dist = distance(xi[column], x[column])
-				print("xi : ", xi[column])
-				print("x : ", x[column])
-				print("dist : ", dist)
-				weight = gauss_kernel(dist, kernel_bandwidth) #Find K(xi - x)
-				print("weight : ", weight)
-				numerator += weight * xi[column]
+				print("xi : ", xi)
+				print("x : ", x)
+				dist = distance(xi[columns], x[columns])
+				weight = gauss_kernel(kernel_bandwidth, dist)
+				print("Distance :", dist)
+				print("Weight: ", weight)
+				# d = (kernel_bandwidth * np.sqrt(2 * math.pi) * np.exp(-0.5 * ((dist / kernel_bandwidth)**2)))
+				
+				# if d == 0:
+				# 	print("d = 0: ")
+				# 	print(xi[columns])
+				# 	print("dist: ", dist)
+
+				# weight = 1 / d
+				numerator += weight * xi[columns]
 				denominator += weight
-				print("numerator : ", numerator)
-				print("denominator : ", denominator)
+				print("\n")
+
 			x_prime = numerator / denominator
-			print("x_prime : ", x_prime)
-			X.loc[i, column] = x_prime
+			# print("x prime: ", x_prime)
+			X.loc[i, columns] = x_prime
 
 		past_X.append(X.copy())
 
@@ -60,14 +73,14 @@ def mean_shift(data, column):
 
 
 data = pd.read_csv('data/monthly_avgs.csv')
-temp_data = data[['Temperature'].iloc[1:30]]
+temp_data = data.iloc[0:30]
 
-mean_shift(temp_data, 'Temperature')
-# dist = distance(temp_data.iloc[0], temp_data.iloc[1])
-# print(temp_data.iloc[7])
-# neighbors = neighborhood_pts(temp_data, temp_data.iloc[7])
-# for n in neighbors:
-# 	print(n)
+
+
+start_time = datetime.now()
+mean_shift(temp_data, ['Temperature', 'Humidity'])
+print("RUN TIME: ", datetime.now() - start_time)
+
 
 
 
