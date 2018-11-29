c439c10bf6c0521024664cceb4bf5ee6d1375470
Henry <hsoule427@gmail.com>
2018-11-28 21:45:08 -0700

    This verison has my own implementation of the distance matrix

--- mean_shift.py
+++ mean_shift.py
@@ -1,9 +1,10 @@
 import pandas as pd
 import numpy as np
 import math
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 from datetime import datetime
 # from sklearn.neighbors import BallTree
+from sklearn.metrics.pairwise import euclidean_distances
 
 kernel_bandwidth = 4
 
@@ -27,19 +28,25 @@ def neighborhood_pts(X, x_centroid, columns, radius=6):
 
 def gauss_kernel(bandwidth, distance):
 	d = (bandwidth * np.sqrt(2 * math.pi) * np.exp(-0.5 * ((distance / bandwidth)**2)))
-	print(d)
 	return (1 / d)
 
-#TODO pass columns array as a parameter
-def mean_shift(data, columns):
+
+def make_plot(data, columns, radius):
+	filename = "../Mean Shift Trials/Points1-200/Radius" + str(radius) + ".png"
+	fig,ax = plt.subplots()
+	ax.plot(data[columns[0]].values, data[columns[1]].values, 'o')
+	plt.savefig(filename)
+
+
+def mean_shift(data, columns, radius):
 	X = data[columns].copy()
 	iterations = 5
 	past_X = []
 	break_loop = False
 	for it in range(iterations):
-		for i,x in X.iterrows():
+		for i,x in X.iterrows(): #For each point in X
 			#Find neighbors
-			neighbors = neighborhood_pts(X, x, columns)
+			neighbors = neighborhood_pts(X, x, columns, radius)
 
 			numerator = 0
 			denominator = 0
@@ -47,36 +54,71 @@ def mean_shift(data, columns):
 			if len(neighbors) == 0: continue
 
 			for neighbor in neighbors:
-				print("neighbor : ", neighbor)
-				print("x : ", x)
 				dist = distance(neighbor[columns], x[columns])
 				weight = gauss_kernel(kernel_bandwidth, dist)
-				print("Distance :", dist)
-				print("Weight: ", weight)
 				numerator += weight * neighbor[columns]
 				denominator += weight
-				print("Numerator: ", numerator)
-				print("Denominator: ", denominator)
-				print("\n")
-			
+			# END LOOP
 			x_prime = numerator / denominator
-			print("x prime: ", x_prime)
-			print("\n")
+			# print("x prime: ", x_prime)
+			# print("\n")
 			X.loc[i, columns] = x_prime
+		# END LOOP
 
 		past_X.append(X.copy())
+	# END LOOP
+	return past_X
+# END FUNCTION
 
-	print(past_X)
+# Vectorized attempt
+def _mean_shift(data, columns):
+	X = data[columns].copy()
+# END FUNCTION
+
+
 
 
 data = pd.read_csv('data/monthly_avgs.csv')
-temp_data = data.iloc[0:30]
+temp_data = data[['Temperature', 'Humidity']].iloc[0:3]
 
 
 
 start_time = datetime.now()
-mean_shift(data, ['Temperature', 'Humidity'])
-print("RUN TIME: ", datetime.now() - start_time)
+
+# for radius in range(2,6):
+# 	print("Radius = ", radius, "...")
+# 	shifted_data = mean_shift(temp_data, ['Temperature', 'Humidity'], radius)
+# 	shifted_df = pd.DataFrame(data=shifted_data[4])
+# 	make_plot(shifted_df, ['Temperature', 'Humidity'], radius)
+
+# print("RUN TIME: ", datetime.now() - start_time)
+
+distance_mat = []
+for i in temp_data.index:
+	for j in temp_data.index:
+		distance_mat.append(distance(temp_data.iloc[i], temp_data.iloc[j]))
+
+distance_mat = np.array(distance_mat)
+distance_mat.shape = (3,3)
+
+print("VERSION 1:")
+print(distance_mat)
+
+# Convert dataframe to ndarray
+arr = temp_data.values
+# expd = np.expand_dims(arr, 2)
+# tiled = np.tile(expd, arr.shape[0])
+# trans = np.transpose(arr)
+# diff = trans - tiled
+# print(trans)
+print("VERSION 2:")
+print(euclidean_distances(arr, arr))
+
+
+
+
+
+
 
 
 
